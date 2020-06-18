2492c0b2c4a0aff97d2b800a81f5feb6e094b586
betuulcan <betuulcan@hotmail.com>
2020-06-18 11:35:51 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'burc_liste.dart';
+import 'burc_detay.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Burc Rehberi",
+      debugShowCheckedModeBanner: false,
+      //ilk rota
+      initialRoute: "/burcListesi",
+      routes: {
+        "/": (context) => BurcListesi(),
+        "/burcListesi": (context) => BurcListesi(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        List<String> pathElemanlari =
+            settings.name.split("/"); //  /  burcDetay  /    1
+        if (pathElemanlari[1] == 'burcDetay') {
+          return MaterialPageRoute(
+              builder: (context) => BurcDetay(int.parse(pathElemanlari[2])));
+        }
+        return null;
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+    );
+  }
+}
